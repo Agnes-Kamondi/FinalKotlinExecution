9e266e4e873de566928f64e52fb371162a3e06da
Agnes-Kamondi <kamondiagnesauma@gmail.com>
2024-10-16 09:54:27 +0300

    Android studio

--- app/src/main/java/com/agnes/multimedia/ui/PhotoCaptureActivity.kt
+++ app/src/main/java/com/agnes/multimedia/ui/PhotoCaptureActivity.kt
@@ -9,48 +9,63 @@ import android.graphics.BitmapFactory
 import android.location.LocationRequest
 import android.os.Bundle
 import android.os.Environment
+import android.os.Looper
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.agnes.multimedia.R
 import com.agnes.multimedia.databinding.ActivityPhotoCaptureBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import java.io.File
 
 class PhotoCaptureActivity : AppCompatActivity() {
-    lateinit var photoFile : File
+    lateinit var photoFile: File
 
-    var cameraLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
-        if(result.resultCode == Activity.RESULT_OK){
-            val capturedPhoto = BitmapFactory.decodeFile(photoFile.absolutePath)
-            binding.ivPhoto2.setImageBitmap(capturedPhoto)
+    var cameraLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val capturedPhoto = BitmapFactory.decodeFile(photoFile.absolutePath)
+                binding.ivPhoto2.setImageBitmap(capturedPhoto)
+            }
         }
-    }
 
     lateinit var binding: ActivityPhotoCaptureBinding
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
-    val locationRequest = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()){result ->
-        if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED){
-            getLocation()
-        }else{
-            Toast.makeText(this,"Provide location permission", Toast.LENGTH_LONG).show()
+    val locationRequest =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+//                && ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)
+//                == PackageManager.PERMISSION_GRANTED
+            ) {
+                getLocationUpdates()
+            }
         }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPhotoCaptureBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
     }
-    private fun getPhotoFile(filename : String): File{
+
+    private fun getPhotoFile(filename: String): File {
         val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(filename, ".jpg",storageDir)
+        return File.createTempFile(filename, ".jpg", storageDir)
     }
 
     override fun onResume() {
@@ -58,13 +73,46 @@ class PhotoCaptureActivity : AppCompatActivity() {
         binding.btnCapture.setOnClickListener {
             val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             photoFile = getPhotoFile("photo_${System.currentTimeMillis()}")
-            val fileProvider = FileProvider.getUriForFile(this,"com.agnes.multimedia.provider",photoFile)
-            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,fileProvider)
+            val fileProvider =
+                FileProvider.getUriForFile(this, "com.agnes.multimedia.provider", photoFile)
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
             cameraLauncher.launch(cameraIntent)
         }
+        getLocationUpdates()
     }
-    fun getLocation(){
-//        if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED)
-//        val requestUserLocation = LocationRequest.Builder(10000).build()
-//    }
+
+    private fun getLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+//            && ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)
+//            == PackageManager.PERMISSION_GRANTED
+        ) {
+            val locationRequest = com.google.android.gms.location.LocationRequest
+                .Builder(10000)
+                .setPriority(Priority.PRIORITY_BALANCED_POWER_ACCURACY)
+                .build()
+            val locationCallback = object : LocationCallback(){
+                override fun onLocationResult(p0: LocationResult) {
+                    super.onLocationResult(p0)
+                    val lastLocation = p0.lastLocation
+                    if(lastLocation!=null){
+                        Toast.makeText(this@PhotoCaptureActivity,
+                            "${lastLocation.latitude},${lastLocation.longitude}",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+
+            fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        }else{
+            locationRequest.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+    }
 }
